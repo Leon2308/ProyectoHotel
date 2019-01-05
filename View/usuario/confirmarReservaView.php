fca8202f116574f82ac09e52381315c42ed82d2e
lesliediaz <lmdiaz36@gmail.com>
2019-01-04 22:46:48 -0500

    vEstilo

--- View/usuario/confirmarReservaView.php
+++ View/usuario/confirmarReservaView.php
@@ -22,12 +22,24 @@
                     <input type="hidden"  name="codHabitacion" value="<?= $codHabitacion ?>">
                     <input type="hidden"  name="codCliente" value="<?= $datosUser->GetCodCliente() ?>">
                     <input type="hidden"  name="probando2" value="test2">
-                    <input type="text"  name="dni" value="<?= $datosUser->GetDNI() ?>" readonly>
-                    <input type="text"  name="nombre" value="<?= $datosUser->GetNombre() ?>" readonly>
-                    <input type="text"  name="apellido1" value="<?= $datosUser->GetApellido1() ?>" readonly>
-                    <input type="text"  name="apellido2" value="<?= $datosUser->GetApellido2() ?>" readonly>
-                    <input type="text"  name="fechaEntrada"  value="<?= $fechaEntrada ?>" readonly>
-                    <input type="text"  name="fechaSalida" value="<?= $fechaSalida ?>" readonly>
+					 <label for="male">Cédula</label>
+                    <input type="text"  name="dni" id="dni" value="<?= $datosUser->GetDNI() ?>" readonly>
+					 <label for="male">Nombre</label>
+                    <input type="text"  name="nombre" id="nombre" value="<?= $datosUser->GetNombre() ?>" readonly>
+					<?php if($datosUser->GetApellido2()!=''){ ?>
+					 <label for="male">Primer Apellido</label>
+                    <input type="text"  name="apellido1" id="apellido1" value="<?= $datosUser->GetApellido1() ?>" readonly>
+					 <label for="male">Segundo Apellido</label>
+                    <input type="text"  name="apellido2" id="apellido2" value="<?= $datosUser->GetApellido2() ?>" readonly>
+					<?php }else { ?>
+					 <label for="male">Apellido</label>
+                    <input type="text"  name="apellido1" id="apellido1" value="<?= $datosUser->GetApellido1() ?>" readonly>
+					<input type="hidden"  name="apellido2" id="apellido2" value="<?= $datosUser->GetApellido2() ?>" readonly>
+					<?php } ?>
+					 <label for="male">Fecha de Entrada</label>
+                    <input type="text"  name="fechaEntrada"  id="fechaEntrada" value="<?= $fechaEntrada ?>" readonly>
+					 <label for="male">Fecha de Salida</label>
+                    <input type="text"  name="fechaSalida" id="fechaSalida" value="<?= $fechaSalida ?>" readonly>
                     <button type="submit">Confirmar</button>
                 </form>
                 <?php
