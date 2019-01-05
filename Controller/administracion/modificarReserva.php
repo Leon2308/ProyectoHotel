27a1b319370332de2feb2262c1487f9c16fd7089
Mishu95 <tefi_0095@outlook.com>
2019-01-04 22:55:05 -0500

    Update modificarReserva.php

--- Controller/administracion/modificarReserva.php
+++ Controller/administracion/modificarReserva.php
@@ -1,10 +1,10 @@
 <?php
-session_start();
-if ($_SESSION['logueadoAdmin'] == true) {
+session_start();   //empezar sesion 
+if ($_SESSION['logueadoAdmin'] == true) {  // si la sesion es iniciada
 require_once 'compruebaDB.php';
 require_once '../../Model/Reserva.php';
 
-$reservaAux = new Reserva($_POST[codHabitacion], $_POST[codCliente], $_POST[fechaEntrada], $_POST[fechaSalida], "", "", "", "");
+$reservaAux = new Reserva($_POST[codHabitacion], $_POST[codCliente], $_POST[fechaEntrada], $_POST[fechaSalida], "", "", "", ""); //se crea un atributo para guardar la reseva de la habitacion 
 
 $reservaAux->modReserva($_POST[fechaEntradaHidden]);
 
