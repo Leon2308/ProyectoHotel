cb40f1154517b916e02e7953f4a7377723f4d164
Leonix23 <leo_andres1995@hotmail.es>
2019-01-30 23:58:38 -0500

    Create ReservaHabitacion.php

---
+++ Model/ReservaHabitacion.php
@@ -0,0 +1,21 @@
+<?php
+include_once 'HotelDB.php';
+/**
+ * Clase para obtener datos de habitación y Cliente.
+ *
+ * @author Moisés
+ */
+class ReservaHabitacion {
+
+    private $codHabitacion;
+    private $tipo;
+    private $capacidad;
+    private $planta;
+    private $tarifa;
+    private $fechaEntrada;
+    private $fechaSalida;
+
+    
+    
+
+}
